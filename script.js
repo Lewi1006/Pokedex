35c5e604eb003d0728bb6f072c6e7cd0f90a7a1d
Lewi1006 <coach.wind11@gmail.com>
2026-06-09 12:52:28 +0200

    feat: filter function

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-// local copy of array
+// local copy of array of Pokemon Cards
 let currentPkmArray = [];
+
+// empty Array 
+let pkmFiltered = [];
 
 function init() {
   getData();
@@ -30,6 +33,9 @@ async function getPkm(arr) {
     console.log(pokemonData);
   }
 
+  // duplicate array
+  pkmFiltered = currentPkmArray;
+  console.log(pkmFiltered);
   renderCard();
 }
 
@@ -37,8 +43,8 @@ function renderCard() {
   const cardRef = document.getElementById(`pkm-card-container`);
   cardRef.innerHTML = "";
 
-  for (let indexCard = 0; indexCard < currentPkmArray.length; indexCard++) {
-    let pokemon = currentPkmArray[indexCard];
+  for (let indexCard = 0; indexCard < pkmFiltered.length; indexCard++) {
+    let pokemon = pkmFiltered[indexCard];
     let colorClass = getTypeClass(pokemon);
     console.log(colorClass);
 
@@ -59,6 +65,38 @@ function renderTypes(pokemon, indexCard) {
 }
 
 
+// change function so that we check if its certain types like water grass fire etc. if not choose alternative
+// we want to prioritize certain types 
+// new local array where we can compare with types array 
+// if name exists 
 function getTypeClass(pokemon) {
+  const preferred = ["grass", "fire", "water", "electric", "psychic", "ground"];
+
+  for (let indexPref = 0; indexPref < preferred.length; indexPref++) {
+    for (let indexTypes = 0; indexTypes < pokemon.types.length; indexTypes++) {
+      if (pokemon.types[indexTypes].type.name === preferred[indexPref]) {
+        return preferred[indexPref];
+      }
+    }
+  }
+
   return pokemon.types[0].type.name;
+}
+
+
+
+//  filter function is predefined and does for loop and so pokemon hére is 
+// not the same value as pokemon in the other functions
+// it just extracts is in the same way as in renderCard() by going through currentPkmArray and filtering it
+// .filter() is a built-in loop method
+// pokemon is just a local parameter name inside that loop
+// it is NOT connected to other pokemon variables elsewhere
+// also we need to go through the full data as in currentPkmArray and not just the Filtered 
+// but filtered willl always e the same as currentPkm till we type in something into input 
+// cause thats when we call the filter function where we assign new value to pkmfiltered 
+// arr and since we call renderCards() after this will be know the renderedCards thats 
+// why renderCards uses pkmFiltered from the start
+function filterAndShowNames(filterWord){
+  pkmFiltered = currentPkmArray.filter(pokemon => pokemon.name.includes(filterWord));
+  renderCard();
 }
